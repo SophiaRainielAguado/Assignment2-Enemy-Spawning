56d9eaa27808cd8eae3d60c946e0f99bcd30721a
Sophia Rainiel Arevalo Aguado <aguadosophiarainiel@gmail.com>
2026-05-17 04:24:14 -0700

    [fix] Spellcaster
    With changes to Spell and SpellBuilder. Logically I need to update Spellcaster as well to match the new Spell API with the paramteres

--- Assets/Scripts/Spells/SpellCaster.cs
+++ Assets/Scripts/Spells/SpellCaster.cs
@@ -2,46 +2,51 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-public class SpellCaster 
+public class SpellCaster
 {
     public int mana;
     public int max_mana;
     public int mana_reg;
     public Hittable.Team team;
+
+    public int spellpower;
+    public int wave;
+
     public Spell spell;
+    private SpellBuilder spellBuilder;
+
+    public SpellCaster(int mana, int max_mana, int mana_reg, Hittable.Team team, SpellBuilder builder)
+    {
+        this.mana = mana;
+        this.max_mana = max_mana;
+        this.mana_reg = mana_reg;
+        this.team = team;
+
+        this.spellBuilder = builder;
+
+        spell = spellBuilder.Build("arcane_bolt", this);
+    }
 
     public IEnumerator ManaRegeneration()
     {
         while (true)
         {
-            mana += mana_reg;
-            mana = Mathf.Min(mana, max_mana);
+            mana = Mathf.Min(mana + mana_reg, max_mana);
             yield return new WaitForSeconds(1);
         }
     }
 
-    public SpellCaster(int mana, int mana_reg, Hittable.Team team)
-    {
-        this.mana = mana;
-        this.max_mana = mana;
-        this.mana_reg = mana_reg;
-        this.team = team;
-        spell = new SpellBuilder().Build(this);
-    }
-
     public IEnumerator Cast(Vector3 where, Vector3 target)
-    {        
-        if (mana >= spell.GetManaCost() && spell.IsReady())
-        {
-            mana -= spell.GetManaCost();
-            yield return spell.Cast(where, target, team);
-        }
-        yield break;
-    }
-
-    public void SetSpell(spelldata data)
     {
-        spell = new Spell(this, data);
+        if (mana >= spell.GetManaCost(spellpower) && spell.IsReady(spellpower, wave))
+        {
+            mana -= spell.GetManaCost(spellpower);
+            yield return spell.Cast(where, target, team, spellpower, wave);
+        }
     }
 
+    public void SetSpell(string spellName)
+    {
+        spell = spellBuilder.Build(spellName, this);
+    }
 }
