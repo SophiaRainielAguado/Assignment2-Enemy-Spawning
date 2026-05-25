b36b40bf57f17542de32644d66590be8e6fedd64
thechirpy <abbygagea27@gmail.com>
2026-05-24 23:31:36 -0700

    Changes to relic cs
    Modified relic class, adding base trigger/effects class

--- Assets/Scripts/Relics/Relics.cs
+++ Assets/Scripts/Relics/Relics.cs
@@ -26,18 +26,88 @@ public class TriggerInfo
     public virtual string? amount;
 
 }
-public abstract class Relic
+public class Relic
 {
     public RelicData data;
     public SpellCaster owner;
 
-    public Relic(SpellCaster owner, RelicData data)
+    public RelicTrigger trigger;
+    public RelicEffect effect;
+
+    public Relic(
+        SpellCaster owner,
+        RelicData data,
+        RelicTrigger trigger,
+        RelicEffect effect)
     {
         this.owner = owner;
         this.data = data;
+
+        this.trigger = trigger;
+        this.effect = effect;
     }
 
-    public abstract void Register();
-    public abstract void Unregister();
+    public void Register()
+    {
+        trigger.Register(this);
+    }
+
+    public void Unregister()
+    {
+        trigger.Unregister(this);
+    }
+
+    public void Activate()
+    {
+        effect.Activate(owner);
+    }
 }
+
+public abstract class RelicEffect
+{
+    public EffectInfo data;
+
+    public RelicEffect(EffectInfo data)
+    {
+        this.data = data;
+    }
+
+    public abstract void Activate(SpellCaster owner);
+}
+
+public class GainManaEffect : RelicEffect
+{
+    public GainManaEffect(EffectInfo data) : base(data)
+    {
+    }
+     public override void Activate(SpellCaster owner, int wave)
+    {
+        var vars = new Dictionary<string, int>()
+        {
+            { "wave", wave},
+            { "power", owner.spellpower}
+        };
+
+        int amount = RPNEvaluator.RPNEvaluator.Evaluate(data.amount, vars);
+
+        owner.mana += amount;
+        owner.mana = Mathf.Min(owner.mana, owner.max_mana);
+    }
+
+
+}
+
+public abstract class TriggerEffect
+{
+    public TriggerInfo data;
+
+    public TriggerEffect(EffectInfo data)
+    {
+        this.data = data;
+    }
+
+    public abstract void Activate(SpellCaster owner);
+}
+
+
 
